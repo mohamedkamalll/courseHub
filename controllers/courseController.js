e93e3602eafaa7cbc7f31a9e8b7fc22b65eb6061
mohamedkamalll <mohamed.kkamalll@gmail.com>
2024-03-15 20:51:25 +0200

    add and delete courses add successfully

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -11,3 +11,28 @@ module.exports.getCourses = async (req,res,next) => {
     }
 
 }
+
+module.exports.addCourse = async(req,res,next) => {
+    try {
+        const {courseName, courseLevel} = req.body
+        console.log(courseName,courseLevel)
+        const pool = await poolPromise
+        await pool.request().query(`INSERT INTO academyCourses (courseName,courseLevel) VALUES ('${courseName}',${courseLevel})`)  
+        return res.status(200).send("added Successfully");
+    } catch (error) {
+        next(error.message)        
+    }
+}
+
+module.exports.deleteCourse = async(req,res,next) => {
+    try {
+        const {courseName} = req.body
+        console.log(courseName)
+        const pool = await poolPromise
+        const responsee = await pool.request().query(`DELETE FROM academyCourses WHERE courseName = '${courseName}'`)  
+        console.log(responsee)
+        return res.status(200).send("deleted Successfully");
+    } catch (error) {
+        next(error.message)        
+    }
+}
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -3,4 +3,6 @@ const Course = require('../controllers/courseController')
 const router = Router()
 
 router.get('/getCourses',Course.getCourses)
+router.post('/addCourse',Course.addCourse)
+router.delete('/deleteCourse',Course.deleteCourse)
 module.exports = router;
